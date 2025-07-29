8f43cabe1f08bd82971182e8caffc65d14237a32
boboiazumi <boboi0704@gmail.com>
2025-07-29 19:23:41 +0700

    fix: docker iissue

--- frontend-service/src/config.ts
+++ frontend-service/src/config.ts
@@ -1,7 +1,7 @@
 const defaultPort = import.meta.env.VITE_MODE == "production" ? null : 4000
 
 export const BACKEND_CONFIG = {
-    protocol: import.meta.env.VITE_PROTOCOL || "http",
-    host: import.meta.env.VITE_HOST || "localhost",
-    port: import.meta.env.VITE_PORT || (defaultPort == null ? undefined : defaultPort)
+    protocol: import.meta.env.VITE_PROTOCOL ?? "http",
+    host: import.meta.env.VITE_HOST ?? "localhost",
+    port: import.meta.env.VITE_PORT ?? (defaultPort == null ? undefined : defaultPort)
 }
